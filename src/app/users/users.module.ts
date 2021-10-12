32861b2ee6865f7691b0e5a23bc39a397d8e9dd4
JorgeAndrewDevelop <jorge.andrew@colorcodestudio.com.mx>
2021-10-12 11:54:14 -0500

    Creacion de componente users

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UsersTableComponent } from './users-table/users-table.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UsersTableComponent
+  ],
   imports: [
     CommonModule
   ]
