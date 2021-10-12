34ce049ac29ea7c068a22061ee61bf2a4a17c0e3
JorgeAndrewDevelop <jorge.andrew@colorcodestudio.com.mx>
2021-10-12 11:56:14 -0500

    Implementacion de Servicio UsersService

--- src/app/services/user-service.service.ts
+++ src/app/services/user-service.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserServiceService {
+export class UsersService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUsers() : Observable<any> {
+    return this.http.get("http://127.0.0.1:8005/api/users");
+  }  
 }
